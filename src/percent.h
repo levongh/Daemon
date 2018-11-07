211d1eb5eb7cebdb36b0c49d72eb526d69d0aa52
levongh <levon_ghukasyan@live.com>
2018-11-07 12:38:35 +0400

    added query string class and separated some clase definitions

---
+++ src/percent.h
@@ -0,0 +1,46 @@
+#pragma once
+
+///@brief Percent encoding and decoding
+class Percent
+{
+public:
+    ///@ Returns percent-encoded string
+    static std::string encode(const std::string& value) noexcept
+    {
+        static auto hexChars = "0123456789ABCDEF";
+        std::string result;
+        result.reserve(value.size()); //!Minimum size of result
+
+        for (auto& chr : value) {
+            if(!((chr >= '0' && chr <= '9') || (chr >= 'A' && chr <= 'Z') || (chr >= 'a' && chr <= 'z') || chr == '-' || chr == '.' || chr == '_' || chr == '~')) {
+                result += std::string("%") + hexChars[static_cast<unsigned char>(chr) >> 4] + hexChars[static_cast<unsigned char>(chr) & 15];
+            }  else {
+                result += chr;
+            }
+        }
+        return result;
+    }
+
+    ///@ Returns percent-decoded string
+    static std::string decode(const std::string& value) noexcept
+    {
+        std::string result;
+        result.reserve(value.size() / 3 + (value.size() % 3)); // Minium size of result
+        for (size_t i = 0; i < value.size(); ++i) {
+            auto& chr = value[i];
+            if (chr == '%' && i +2 < value.size()) {
+                auto hex = value.substr(i + 1, 2);
+                auto decodedChr = static_cast<char>(std::strtol(hex.c_str(), nullptr, 16));
+                result += decodedChr;
+                i += 2;
+            } else if (chr == '+') {
+                result += ' ';
+            } else {
+                result += chr;
+            }
+        }
+        return result;
+    }
+
+};
+
---
+++ src/querystring.h
@@ -0,0 +1,57 @@
+#pragma once
+
+///@brief Query string creation and parsing
+class QueryString
+{
+public:
+    ///@brief query string created from given field names and values
+    static std::string create(const CaseInsensitiveMultimap& fields) noexcept
+    {
+        std::string result;
+        bool first = true;
+        for (auto& field : fields) {
+            result += (!first ? "&" : "") + field.first + '=' + Percent::encode(field.second);
+            first = false;
+        }
+        return result;
+    }
+
+    static CaseInsensitiveMultimap parse(const std::string& queryString) noexcept
+    {
+        CaseInsensitiveMultimap result;
+        if (queryString.empty()) {
+            return result;
+        }
+        size_t namePos = 0;
+        auto nameEndPos = std::string::npos;
+        auto valuePos = std::string::npos;
+        for (size_t c = 0; c < queryString.size(); ++c) {
+            if (queryString[c] == '&') {
+                auto name = queryString.substr(namePos,
+                        (nameEndPos == std::string::npos ? c : nameEndPos) - namePos);
+                if (!name.empty()) {
+                    auto value = valuePos == std::string::npos ?
+                        std::string() : queryString.substr(valuePos, c - valuePos);
+                        result.emplace(std::move(name), Percent::decode(value));
+                }
+                namePos = c + 1;
+                nameEndPos = std::string::npos;
+                valuePos = std::string::npos;
+            } else if (queryString[c] == '=') {
+                nameEndPos= c;
+                valuePos = c + 1;
+            }
+        }
+        if (namePos < queryString.size()) {
+            auto name = queryString.substr(namePos, nameEndPos - namePos);
+            if (!name.empty()) {
+                auto value = valuePos >= queryString.size() ? std::string() : queryString.substr(valuePos);
+                result.emplace(std::move(name), Percent::decode(value));
+            }
+        }
+        return result;
+    }
+
+};
+
+
