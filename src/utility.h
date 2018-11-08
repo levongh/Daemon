d150060c2edcaaffe1ff556f16ac326b3bae4fd5
levongh <levon_ghukasyan@live.com>
2018-11-08 11:11:34 +0400

    moved to separate file

--- src/utility.h
+++ src/utility.h
@@ -1,42 +1,7 @@
 #pragma once
 
 #include "statuscode.h"
-
-namespace server {
-
-class CaseInsensitiveEqual
-{
-public:
-
-    static bool caseInsensitiveEqual(const std::string& lhs,
-                                     const std::string& rhs) noexcept;
-
-    bool operator()(const std::string& lhs, const std::string& rhs) const noexcept
-    {
-        return CaseInsensitiveEqual::caseInsensitiveEqual(lhs, rhs);
-    }
-};
-
-// Based on https://stackoverflow.com/questions/2590677/how-do-i-combine-hash-values-in-c0x/2595226#2595226
-class CaseInsensitiveHash
-{
-public:
-    size_t operator()(const std::string& str) const noexcept
-    {
-        size_t h = 0;
-        std::hash<int> hash;
-        for (auto c : str) {
-            h ^= hash(std::tolower(c)) + 0x9e3779b9 + (h << 6) + (h >> 2);
-        }
-        return h;
-    }
-};
-
-using CaseInsensitiveMultimap = std::unordered_map<std::string, std::string, CaseInsensitiveHash, CaseInsensitiveEqual>;
-
-} //namespace server
-
-
+#include "util/caseinsentitive.h"
 #include "util/percent.h"
 #include "util/querystring.h"
 #include "util/httpheader.h"
