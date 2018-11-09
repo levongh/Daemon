4336a87a1f71eedd4b859ee127136fded202f53f
levongh <levon_ghukasyan@live.com>
2018-11-09 10:10:13 +0400

    added class request message

--- src/util/message.h
+++ src/util/message.h
@@ -2,9 +2,65 @@
 
 namespace server {
 
+class RequestMessage
+{
+public:
+    /// Parse request line and header fields
+    static bool parse(std::istream& stream,
+                      std::string& method,
+                      std::string& path,
+                      std::string& queryString,
+                      std::string& version,
+                      CaseInsensitiveMultimap& header) noexcept
+    {
+        header.clear();
+        std::string line;
+        getline(stream, line);
+        size_t methodEnd;
+        if ((methodEnd = line.find(' ')) != std::string::npos) {
+            method = line.substr(0, methodEnd);
+            size_t queryStart = std::string::npos;
+            size_t pathAndQueryStringEnd = std::string::npos;
+            for (size_t i = methodEnd + 1; i < line.size(); ++i) {
+                if (line[i] == '?' && (i + 1) < line.size()) {
+                    queryStart = i + 1;
+                } else if (line[i] == ' ') {
+                    pathAndQueryStringEnd = i;
+                    break;
+                }
+            }
+            if (pathAndQueryStringEnd != std::string::npos) {
+                if (queryStart != std::string::npos) {
+                    path = line.substr(methodEnd + 1, pathAndQueryStringEnd - methodEnd - 2);
+                    queryString = line.substr(queryStart, pathAndQueryStringEnd - queryStart);
+                } else {
+                    path = line.substr(methodEnd + 1, pathAndQueryStringEnd= methodEnd - 1);
+                }
+
+                size_t protocolEnd;
+                if ((protocolEnd = line.find('/', pathAndQueryStringEnd + 1)) != std::string::npos) {
+                    if (line.compare(pathAndQueryStringEnd + 1, protocolEnd - pathAndQueryStringEnd - 1, "HTTP") != 0) {
+                        return false;
+                    }
+                    version = line.substr(protocolEnd + 1, line.size() - protocolEnd - 2);
+                } else {
+                    return false;
+                }
+                header = HttpHeader::parse(stream);
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+        return true;
+    }
+};
+
 class ResponseMessage
 {
 public:
+    ///@brief Parse status line and header fields
     static bool parse(std::istream& stream,
                       std::string& version,
                       std::string& statusCode,
