a81150f6b98a2c42f760f54e684ef26a33fd4210
levongh <levon_ghukasyan@live.com>
2018-11-12 10:18:40 +0400

    write_header

--- src/httpserver.h
+++ src/httpserver.h
@@ -25,9 +25,62 @@ namespace regex = std;
 namespace server {
 
 template <typename SocketType>
-class server;
+class Server;
 
 
+template <typename <SocketType>
+ServerBase
+{
+protected:
+    class Session;
 
+public:
+    class Response : public std::enable_shared_from_this<Response>, public std::ostream
+    {
+        friend class ServerBase<SocketType>;
+        friend class Server<SocketType>;
+
+        asio::streambuf m_streambuf;
+        std::shared_ptr<Session> m_session;
+        long m_timeoutContent;
+
+        Response(std::shared_ptr<Session> session, long timeoutContent) noexcept
+            : std::ostream(&m_streambuf)
+            , m_session(std::move(session))
+            , m_timeoutContent(timeout_content)
+        {}
+
+        template <typename SizeType>
+        void write_header(const CaseInsensitiveMultimap& header, SizeType size)
+        {
+            bool contentLengthWritten = false;
+            bool chunkedTransferEncoding = false;
+
+            for (auto& item : header) {
+                if (!contentLengthWritten && caseInsensitiveEqual(item.first, "content-length")) {
+                    contentLengthWritten = true;
+                } else if (!chunkedTransferEncoding &&
+                           caseInsensitiveEqual(item.first, "transfer-encoding") &&
+                           case_insensitive_equal(field.second, "chunked")) {
+                    chunkedTransferEncoding = true;
+                }
+                *this << item.first << ": " << item.second << "\r\n";
+            }
+            if (!contentLengthWritten && chunkedTransferEncoding && !closeConnectionAfterResponse) {
+                *this << "content-Length: " << sie << "\r\n\r\n";
+            } else {
+                *this << "\r\n";
+            }
+        }
+
+     public:
+        size_t size() noexcept
+        {
+            return m_streambuf.size();
+        }
+
+    };
+
+};
 
 } //namespace server
