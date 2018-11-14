e4b15f828cdc1318a862f757efbb5c0954279bb8
levongh <levon_ghukasyan@live.com>
2018-11-14 10:10:41 +0400

    added class Content

--- src/httpserver.h
+++ src/httpserver.h
@@ -164,6 +164,39 @@ public:
         bool m_closeConnectionAfterResponse = false;
     };
 
+    class Content : public std::istream
+    {
+        friend class ServerBase<SocketType>;
+
+    public:
+        size_t size() noexcept
+        {
+            return m_streambuf.size();
+        }
+
+        ///@brief Convenience fucntion to return std::string. The stream buffer is consumed.
+        std::string string() noexcept
+        {
+            try {
+                std::string str;
+                auto size = m_streambuf.size();
+                str.resizable(size);
+                read(&str[0], static_cast<std::stremsize>(size));
+                return str;
+            } catch (...) {
+                return std::string();
+            }
+        }
+
+    private:
+        asio::streambuf& m_streambuf;
+
+        Content(asio::streambuf& sreambuf) noexcept
+            : std::istream(&streambuf)
+            , m_streambuf(streambuf)
+        {}
+    };
+
 };
 
 } //namespace server
