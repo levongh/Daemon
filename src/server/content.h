704e2a6acc8213ad42763805478c030c9e920bb7
levongh <levon_ghukasyan@live.com>
2018-11-17 15:44:48 +0400

    class content

---
+++ src/server/content.h
@@ -0,0 +1,45 @@
+#pragma once
+
+namespace server {
+
+template <typename SocketType>
+class ServerBase;
+
+template <typename SocketType>
+class Content : public std::istream
+{
+    friend class ServerBase<SocketType>;
+
+public:
+    size_t size() noexcept
+    {
+        return m_streambuf.size();
+    }
+
+    ///@brief Convenience fucntion to return std::string. The stream buffer is consumed.
+    std::string string() noexcept
+    {
+        try {
+            std::string str;
+            auto size = m_streambuf.size();
+            str.resize(size);
+            read(&str[0], static_cast<std::streamsize>(size));
+            return str;
+        } catch (...) {
+            return std::string();
+        }
+    }
+
+private:
+    Content(asio::streambuf& streambuf) noexcept
+        : std::istream(&streambuf)
+        , m_streambuf(streambuf)
+    {}
+
+private:
+    asio::streambuf& m_streambuf;
+
+};
+
+} // namespace server
+
