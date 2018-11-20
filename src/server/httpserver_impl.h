21ef04af5cb78453a7a8a071268621eef951b387
levongh <levon_ghukasyan@live.com>
2018-11-20 17:05:07 +0400

    added function definitions

--- src/server/httpserver_impl.h
+++ src/server/httpserver_impl.h
@@ -71,8 +71,8 @@ void ServerBase<SocketType>::acceptAndRun()
         }
     }
 }
-    
-    template <typename SocketType>
+
+template <typename SocketType>
 unsigned short ServerBase<SocketType>::bind()
 {
     asio::ip::tcp::endpoint endpoint;
@@ -100,11 +100,11 @@ unsigned short ServerBase<SocketType>::bind()
 
 template <typename SocketType>
 template <typename... Args>
-std::shared_ptr<Connection<SocketType> > ServerBase<SocketType>::createConnection(Args&&... args) noexcept
+auto ServerBase<SocketType>::createConnection(Args&&... args) noexcept -> ConnectionPtr
 {
     auto connections = m_connections;
     auto connectionsMutex = m_connectionsMutex;
-    auto connection = std::shared_ptr<Connection<SocketType> >(
+    auto connection = ConnectionPtr(
             new Connection<SocketType> (m_handlerRunner, std::forward<Args>(args) ...),
             [connections, connectionsMutex] (Connection<SocketType>* connection) {
             std::unique_lock<std::mutex> lock(*connectionsMutex);
@@ -123,7 +123,7 @@ std::shared_ptr<Connection<SocketType> > ServerBase<SocketType>::createConnectio
 }
 
 template <typename SocketType>
-void ServerBase<SocketType>::read(const std::shared_ptr<Session<SocketType> >& session)
+void ServerBase<SocketType>::read(const SessionPtr& session)
 {
     session->m_connection->set_timeout(m_config.m_timeoutRequest);
     asio::async_read_until(*session->m_connection->m_socket,
@@ -206,7 +206,7 @@ void ServerBase<SocketType>::read(const std::shared_ptr<Session<SocketType> >& s
             } else if ((headerIt = session->m_request->m_header.find("Transfer-Encoding")) != session->m_request->m_header.end() &&
                     headerIt->second == "chunked") {
                 auto chunksStreambuf = std::make_shared<asio::streambuf>(m_config.m_maxRequestStreambufSize);
-                read_cunked_transfer_encoded(session, chunksStreambuf);
+                readChunkedTransferEncoded(session, chunksStreambuf);
             } else {
                 findResource(session);
             }
@@ -215,3 +215,196 @@ void ServerBase<SocketType>::read(const std::shared_ptr<Session<SocketType> >& s
         }
     });
 }
+
+template <typename SocketType>
+void ServerBase<SocketType>::readChunkedTransferEncoded(const SessionPtr& session,
+                                                        const std::shared_ptr<asio::streambuf> &chunksStreambuf)
+{
+    session->m_connection->set_timeout(m_config.m_timeoutContent);
+    asio::async_read_until(*session->m_connection->m_socket,
+                            session->m_request->m_streambuf,
+                            "\r\n", [this, session, chunksStreambuf] (const error_code &ec, size_t bytesTransferred) {
+        session->m_connection->cancel_timeout();
+        auto lock = session->m_connection->m_handlerRunner->continue_lock();
+        if(!lock) {
+            return;
+        }
+        if((!ec || ec == asio::error::not_found) &&
+                session->m_request->m_streambuf.size() == session->m_request->m_streambuf.max_size()) {
+            auto response = ResponsePtr(new Response<SocketType>(session, m_config.m_timeoutContent));
+            response->write(StatusCode::client_error_payload_too_large);
+            response->send();
+            if(m_onError) {
+                m_onError(session->m_request, make_error_code::make_error_code(errc::message_size));
+            }
+            return;
+        }
+        if(!ec) {
+            std::string line;
+            getline(session->m_request->m_content, line);
+            bytesTransferred -= line.size() + 1;
+            line.pop_back();
+            unsigned long length = 0;
+            try {
+                length = stoul(line, 0, 16);
+            } catch(...) {
+                if(m_onError) {
+                    m_onError(session->m_request, make_error_code::make_error_code(errc::protocol_error));
+                }
+                return;
+            }
+
+            auto numAdditionalBytes = session->m_request->m_streambuf.size() - bytesTransferred;
+            if((2 + length) > numAdditionalBytes) {
+                session->m_connection->set_timeout(m_config.m_timeoutContent);
+                asio::async_read(*session->connection->socket,
+                                  session->request->streambuf,
+                                  asio::transfer_exactly(2 + length - numAdditionalBytes),
+                                  [this, session, chunksStreambuf, length](const error_code &ec, size_t) {
+                    session->connection->cancel_timeout();
+                    auto lock = session->connection->handler_runner->continue_lock();
+                    if(!lock) {
+                        return;
+                    }
+                    if(!ec) {
+                        if(session->m_request->m_streambuf.size() == session->m_request->m_streambuf.max_size()) {
+                            auto response = ResponsePtr(new Response<SocketType>(session, m_config.m_timeoutContent));
+                            response->write(StatusCode::client_error_payload_too_large);
+                            response->send();
+                        if(m_onError) {
+                           m_onError(session->m_request, make_error_code::make_error_code(errc::message_size));
+                        }
+                        return;
+                        }
+                        readChunkedTransferEncodedChunk(session, chunksStreambuf, length);
+                    }  else if(m_onError) {
+                        m_onError(session->m_request, ec);
+                    }
+                });
+            } else {
+                readChunkedTransferEncodedChunk(session, chunksStreambuf, length);
+            }
+        } else if(m_onError) {
+            m_onError(session->m_request, ec);
+        }
+    });
+}
+
+template <typename SocketType>
+void ServerBase<SocketType>::readChunkedTransferEncodedChunk(const SessionPtr& session,
+                                                             const std::shared_ptr<asio::streambuf> &chunksStreambuf,
+                                                             unsigned long length)
+{
+    std::ostream tmpStream(chunksStreambuf.get());
+    if(length > 0) {
+        std::unique_ptr<char[]> buffer(new char[length]);
+        session->m_request->m_content.read(buffer.get(), static_cast<std::streamsize>(length));
+        tmpStream.write(buffer.get(), static_cast<std::streamsize>(length));
+        if(chunksStreambuf->size() == chunksStreambuf->max_size()) {
+            auto response = ResponsePtr(new Response<SocketType>(session, m_config.m_timeoutContent));
+            response->write(StatusCode::client_error_payload_too_large);
+            response->send();
+            if(m_onError) {
+                m_onError(session->m_request, make_error_code::make_error_code(errc::message_size));
+            }
+            return;
+        }
+    }
+
+    ///@brief Remove "\r\n"
+    session->request->content.get();
+    session->request->content.get();
+
+    if(length > 0) {
+        readChunkedTransferEncoded(session, chunksStreambuf);
+    } else {
+        if(chunksStreambuf->size() > 0) {
+            std::ostream ostream(&session->request->streambuf);
+            ostream << chunksStreambuf.get();
+        }
+        findResource(session);
+    }
+}
+
+template <typename SocketType>
+void ServerBase<SocketType>::findResource(const SessionPtr& session)
+{
+    // Upgrade connection
+    if(m_onUpgrade) {
+        auto it = session->m_request->m_header.find("Upgrade");
+        if(it != session->m_request->m_header.end()) {
+            // remove connection from connections
+            {
+                std::unique_lock<std::mutex> lock(*m_connectionsMutex);
+                auto it = m_connections->find(session->m_connection.get());
+                if(it != m_connections->end()) {
+                    m_connections->erase(it);
+                }
+            }
+            m_onUpgrade(session->m_connection->m_socket, session->m_request);
+            return;
+        }
+    }
+    // Find path- and method-match, and call write
+    for(auto &regexMethod : m_resource) {
+        auto it = regexMethod.second.find(session->m_request->m_method);
+        if(it != regexMethod.second.end()) {
+            regex::smatch smRes;
+            if(regex::regex_match(session->m_request->m_path, smRes, regexMethod.first)) {
+                session->m_request->pathMatch = std::move(smRes);
+                write(session, it->second);
+                return;
+            }
+        }
+    }
+    auto it = m_defaultResource.find(session->m_request->m_method);
+    if(it != m_defaultResource.end()) {
+        write(session, it->second);
+    }
+}
+
+template <typename SocketType>
+void ServerBase<SocketType>::write(const SessionPtr& session,
+           Callback &resourceFunction)
+{
+    session->m_connection->set_timeout(m_config.m_timeoutContent);
+    auto response = ResponsePtr(new Response<SocketType>(session, m_config.m_timeoutContent),
+        [this](ResponsePtr *response_ptr) {
+        auto response = ResponsePtr(response_ptr);
+        response->send([this, response](const error_code &ec) {
+            if(!ec) {
+                if(response->m_closeConnectionAfterResponse) {
+                    return;
+                }
+
+                auto range = response->m_session->m_request->m_header.equal_range("Connection");
+                for(auto it = range.first; it != range.second; it++) {
+                    if(CaseInsensitiveEqual::caseInsensitiveEqual(it->second, "close")) {
+                        return;
+                    } else if(CaseInsensitiveEqual::caseInsensitiveEqual(it->second, "keep-alive")) {
+                        auto newSession = std::make_shared<Session>(m_config.m_maxRequestStreambufSize, response->m_session->m_connection);
+                        read(newSession);
+                        return;
+                    }
+                }
+                if(response->m_session->m_request->m_httpVersion >= "1.1") {
+                    auto newSession = std::make_shared<Session>(m_config.m_maxRequestStreambufSize, response->m_session->m_connection);
+                    this->read(newSession);
+                    return;
+                }
+            } else if(this->on_error) {
+                this->on_error(response->session->request, ec);
+            }
+        });
+    });
+
+    try {
+        resourceFunction(response, session->m_request);
+    } catch(const std::exception &) {
+        if(m_onError) {
+            m_onError(session->m_request, make_error_code::make_error_code(errc::operation_canceled));
+        }
+        return;
+    }
+}
+
